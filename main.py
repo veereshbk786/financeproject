cb150b4d508f468f857f6cdcdc5be34bef6aff1f
veeresh babu <veereshk.bigdata@gmail.com>
2025-04-03 09:48:43 +0530

    modified code in main function

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ if __name__ == '__main__':
     friend_df.show(truncate=False)
     movie_df = readFileFunc(spark, args.movie)
     movie_df.show(truncate=False)
+    l = [10, 20, 30, 40, 200, 50, 60, 70, 80]
+    print(max(l),l.index(max(l)))
+    l1 = l.copy()
+    min(l)
+    print("sort : ",l1.sort())
+    print("sort l : ",l)
+    print("index and high ",l1[-1],l.index(l1[-1]))
+    s = {(10,20):'durga'}
+    x = 0
+    for i in l:
+        for j in l:
+            if i < j:
+               # print(x)
+                x = j
+    print(x,l.index(x))
--- test/unit/tesFunc.py
+++ test/unit/tesFunc.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
 
-
+from sparkFunctions.transformationFunc import *
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
